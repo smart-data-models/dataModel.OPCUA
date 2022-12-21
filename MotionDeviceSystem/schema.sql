59315e5197b076337789b5edbdc7363306925e61
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:10:27 +0100

    test of SQL export

---
+++ MotionDeviceSystem/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model MotionDeviceSystem of the subject dataModel.OPCUA for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE MotionDeviceSystem_type AS ENUM ('MotionDeviceSystem');
+CREATE TABLE MotionDeviceSystem (address json, alternateName text, areaServed text, controllers json, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, motionDevices json, name text, owner json, seeAlso json, source text, type MotionDeviceSystem_type);
