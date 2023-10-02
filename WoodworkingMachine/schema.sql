ae245ca01bcb614b680f71acd7ed2ad6b020baad
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:53:22 +0200

    test of SQL export

--- WoodworkingMachine/schema.sql
+++ WoodworkingMachine/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model WoodworkingMachine of the subject dataModel.OPCUA for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model WoodworkingMachine of the subject dataModel.OPCUA for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WoodworkingMachine_type AS ENUM ('WoodworkingMachine');
-CREATE TABLE WoodworkingMachine (Machines json, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type WoodworkingMachine_type);
+CREATE TABLE WoodworkingMachine (Machines JSON, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type WoodworkingMachine_type);
